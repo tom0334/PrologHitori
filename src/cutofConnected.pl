a3b133eaf1690de597e5ccf94f948bfac98462e2
Tom Friederich <tfriederich@live.nl>
2026-01-09 15:47:09 +0100

    Rename a few more variables

--- src/cutofConnected.pl
+++ src/cutofConnected.pl
@@ -3,7 +3,7 @@
 %Params: 
 %[StartPositions]: A list of newly chosen positions(X,Y) to make black. These are the start positions to do DFS from to look for cutting off paths
 %N : size of board, used to detect if a position is on the edge of the board or not.
-%ChosenPositions: A list of positions (X,Y) that were in previous iterations, and that we already know do not cut off anything
+%[ChosenPositions]: A list of positions (X,Y) that were in previous iterations, and that we already know do not cut off anything
 %Run DFS from all startpositions.  
 isStillConnectedFast([], _N, _ChosenPositions).
 
@@ -20,45 +20,45 @@ isStillConnectedFast([Start | TStartPositions], N, ChosenPositions) :-
 
 %This predicate will FAIL when it finds one of these things, and succeed when it ensures the black squares chosen are OK.
 %Params:
-%Queue A list of edges to explore. The start has itself as FROM, but that does not really matter
+%[Queue] A list of edges to explore. The start has itself as FROM, but that does not really matter
 %N: board size
-%ChosenPositions: Positions chosen in previous iterations to be black. These can be explored.
-%SidesOfBoardFoundSoFar the number of sides of the board found so far. When this reaches 2 we fail.
-%UsedEdges: A set of edges that we explored so far, to avoid it going backwards.
-%VisitedNodes: All nodes that we explored so far
+%[ChosenPositions]: Positions chosen in previous iterations to be black. These can be explored.
+%[SidesOfBoardFoundSoFar] the number of sides of the board found so far. When this reaches 2 we fail.
+%[UsedEdges]: A set of edges that we explored so far, to avoid it going backwards.
+%[VisitedNodes]: All nodes that we explored so far
 
 %If the current edge is already used, that means we already considered this edge, so we can do nothing and continue...
-dfsCutSearch([(Head,From) | TQueue], N, ChosenPositions, SidesOfBoardFoundSoFar,UsedEdges,VisitedNodes):-
-    member( (From, Head), UsedEdges), %list with member is faster than ord_set here since these are often small lists. I tested it!
-    dfsCutSearch(TQueue, N, ChosenPositions, SidesOfBoardFoundSoFar, UsedEdges,VisitedNodes),
+dfsCutSearch([(CurrentNode, From) | TQueue], N, ChosenPositions, SidesOfBoardFoundSoFar, UsedEdges, VisitedNodes):-
+    member( (From, CurrentNode), UsedEdges), %list with member is faster than ord_set here since these are often small lists. I tested it!
+    dfsCutSearch(TQueue, N, ChosenPositions, SidesOfBoardFoundSoFar, UsedEdges, VisitedNodes),
     !.
 
 
 %End case 1: when we encounter a position on the side of the board, AND we already found one,
 % There is a cutting of path!
 % we know for sure there is a path from start to two sides, so there is a cutof path!
-dfsCutSearch( [ (Head,_From) | _TQueue], N, _ChosenPositions, 1 , _UsedEdges,_VisitedNodes):-
-    isOnEdge(Head,N),
+dfsCutSearch( [ (CurrentNode, _From) | _TQueue], N, _ChosenPositions, 1 , _UsedEdges, _VisitedNodes):-
+    isOnSideOfBoard(CurrentNode,N),
     !,
     fail.
 
 %End case 2: when we encounter a position that was already VisitedNodes, that means we found a cycle!
 % So there is at least white element not connected to the rest!
-dfsCutSearch([(Head,_From) | __TQueue], _N, _ChosenPositions, _SidesOfBoardFoundSoFar, _UsedEdges,VisitedNodes):-
-    member(Head, VisitedNodes),
+dfsCutSearch([(CurrentNode, _From) | _TQueue], _N, _ChosenPositions, _SidesOfBoardFoundSoFar, _UsedEdges, VisitedNodes):-
+    member(CurrentNode, VisitedNodes),
     !,
     fail.
 
 % End case 3: when the queue is empty, we can say we did NOT find a cutting path. So we do succeed!
-dfsCutSearch([], _N, _ChosenPositions, _SidesOfBoardFoundSoFar, _UsedEdges,_VisitedNodes):-
+dfsCutSearch([], _N, _ChosenPositions, _SidesOfBoardFoundSoFar, _UsedEdges, _VisitedNodes):-
     !.
 
 %Regular case: increment the counter for sidesOfBoard found if needed, find all neigbouring edges to explore, and call yourself recursively.
-dfsCutSearch([(Head,From) | TQueue], N, ChosenPositions, SidesOfBoardFoundSoFar,UsedEdges,VisitedNodes):-
-    calcNewSidesOfBoardFoundSoFar(Head, N, SidesOfBoardFoundSoFar, NewSidesOfBoardFoundSoFar),
-    findAllPositionsToExplore(ChosenPositions, Head, Neighbors),
+dfsCutSearch([(CurrentNode, From) | TQueue], N, ChosenPositions, SidesOfBoardFoundSoFar, UsedEdges, VisitedNodes):-
+    calcNewSidesOfBoardFoundSoFar(CurrentNode, N, SidesOfBoardFoundSoFar, NewSidesOfBoardFoundSoFar),
+    findAllPositionsToExplore(ChosenPositions, CurrentNode, Neighbors),
     append(TQueue, Neighbors, Queue),
-    dfsCutSearch(Queue, N, ChosenPositions, NewSidesOfBoardFoundSoFar, [(Head,From)|UsedEdges],[Head|VisitedNodes]),
+    dfsCutSearch(Queue, N, ChosenPositions, NewSidesOfBoardFoundSoFar, [(CurrentNode,From)|UsedEdges], [CurrentNode|VisitedNodes]),
     !.
 
 
@@ -71,7 +71,7 @@ diagonallyAdjacentPos((X1,Y1),(X2,Y2)) :-
     (X2 is X1 - 1, Y2 is Y1 + 1);
     (X2 is X1 - 1, Y2 is Y1 - 1).
 
-isOnEdge((X,Y),N):- 
+isOnSideOfBoard((X,Y),N):- 
     (X = 0,!);
     (Y = 0,!);
     (X is N -1,!);
@@ -91,7 +91,7 @@ findAllPositionsToExplore(ChosenPositions,  From, AllAdjacentFromInfoPairs) :-
 
 %Adds one to the counter if it is on the edge of the board...
 calcNewSidesOfBoardFoundSoFar(Pos, N, SidesOfBoardFoundSoFar,NewSidesOfBoardFoundSoFar):-
-    isOnEdge(Pos,N),
+    isOnSideOfBoard(Pos,N),
     NewSidesOfBoardFoundSoFar is SidesOfBoardFoundSoFar + 1,
     !.
 
