4d0295ca7184c90d04eba7e52ba1fcceb8f0a16b
Tom Friederich <tfriederich@live.nl>
2026-05-15 17:29:58 +0200

    Cleanup duplicateFinding.pl

--- src/duplicateFinding.pl
+++ src/duplicateFinding.pl
@@ -1,23 +1,26 @@
-%This file contains predicates for finding duplicates in list in an efficient way
+% This file contains predicates for finding duplicates in list in an efficient way
 
-
-% Finding duplicates in a list by creating a dictonary with the amount of times a number occurs
-%%%%%%%%%%
-%creates a dictionary that has the following values:
+% Finds duplicates in a list by creating a dictonary with the amount of times a number occurs
+% creates a dictionary that has the following values:
 % key: the value of the position
-% Value: the number of times that number occurs in the list
-% so countInList([1,1,1,4,7], Result) gives {1:3, 4:1, 7:1} 
+% value: the number of times that number occurs in the list
+% So countInList([1,1,1,4,7], Result) gives {1:3, 4:1, 7:1} 
 countInList(PositionList, ResultDict):-
     dict_create(EmptyDict, _, []),
     countOccurancesIn(PositionList,EmptyDict, ResultDict).
 
+% Counts the occurances of all numbers in a list, generates a dictonary with the counts. A CountMap!
 countOccurancesIn([], Result, Result).
 countOccurancesIn([(_,_,V)|Tail], CountMap, Result):-
-    OldCount = CountMap.get(V, 0),   % 0 is default value, if its not in the map yet. We will bump that up to 0, so we only get 1 if its in there multiple times.
-    NewCount is OldCount + 1, % 
+    % 0 is default value, if its not in the map yet.
+    OldCount = CountMap.get(V, 0),   
+    NewCount is OldCount + 1,
     put_dict(V, CountMap, NewCount, NewDict),
     countOccurancesIn(Tail, NewDict, Result).
 
+
+
+% Uses a CountMap to find which positions are duplicates.
 findDuplicatePositions(PositionList, Dict, DuplicatesOnly):-
     include(positionIsDuplicateAccordingToDict(Dict), PositionList, DuplicatesOnly).
 
@@ -25,27 +28,27 @@ positionIsDuplicateAccordingToDict(Dict, (_,_,V)) :-
     get_dict(V, Dict, Count),
     Count > 1.
 
+
+% Finds the duplicate positions (X,Y,V) in a list, returns only a list of values of the positions that are duplicate.
+% So, ([(1,2,3),(1,1,3),(7,8,9)]) will give [3], since there are 2 positions with value 3 so it is duplicate.
 dupValuesOnly(Result, [], Result).
 
 dupValuesOnly( SoFar, [ (_,_,V) | Tail ], Result):-
     ord_add_element(SoFar,V, NewSoFar),
     dupValuesOnly(NewSoFar, Tail,Result ).
 
-positionIsInRowY(Y, (_, RY, _)) :-
-    RY = Y.
 
-positionIsInColumnX(X, (CX, _, _)) :-
-    CX = X.
 
-onlyPositionsInRowY(Positions, Y, Filtered) :-
-    include(positionIsInRowY(Y), Positions, Filtered).
+% Some helper predicates to find all positions on the board, 
+% and to convert those positions to lists of positions corresponding to each row and column.
+% These are inefficient, but they are only used once per puzzle so that is OK.
 
-onlyPositionsInColumnX(Positions, X, Filtered) :-
-    include(positionIsInColumnX(X), Positions, Filtered).
+% Gives you all positions on the board with value: [(X,Y,V)]
+allPositionsWithValue(Board, PositionsWithValue) :-
+    findall( (X,Y,V), elementAt(Board,X,Y,V), PositionsWithValue).
 
-%This is not an efficient way to do this, so please dont use this in performance critial parts of the model
-%Just gives you 0 1 2 3if your board is size 4.
-% assumes the board is square!
+% Just gives you [0, 1, 2, 3] if your board is size 4.
+% Assumes the board is square!
 allIndices(Positions, Sorted) :-
     findall(X, member((X, _, _), Positions), AllIndices),
     sort(AllIndices, Sorted).
@@ -58,6 +61,18 @@ allRows(Positions, RowList):-
     allIndices(Positions, RowIndices),  % get 0,1,2,3...
     maplist(onlyPositionsInRowY(Positions), RowIndices, RowList).
 
-% gives you the indices as pairs (X,Y) that have a nonzero value at the board
-allPositionsWithValue(Board, PositionsWithValue) :-
-    findall( (X,Y,V), elementAt(Board,X,Y,V), PositionsWithValue).
+
+
+%Some simple helper predicates to find only positions in a certain row or column
+onlyPositionsInRowY(Positions, Y, Filtered) :-
+    include(positionIsInRowY(Y), Positions, Filtered).
+
+onlyPositionsInColumnX(Positions, X, Filtered) :-
+    include(positionIsInColumnX(X), Positions, Filtered).
+
+positionIsInRowY(Y, (_, RY, _)) :-
+    RY = Y.
+
+positionIsInColumnX(X, (CX, _, _)) :-
+    CX = X.
+
